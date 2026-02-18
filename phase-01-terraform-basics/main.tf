379b02d0fd324a5e705abe668e32a3a49ca5dc0d
SivasubramaniamMurugesan <35694369+SivasubramaniamMurugesan@users.noreply.github.com>
2026-02-19 03:28:09 +0530

    updated indentation in main.tf

--- phase-01-terraform-basics/main.tf
+++ phase-01-terraform-basics/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = var.instance_type
-  key_name = var.key_name
+  key_name      = var.key_name
 
   tags = {
     Name = "Devops-Project-01"
